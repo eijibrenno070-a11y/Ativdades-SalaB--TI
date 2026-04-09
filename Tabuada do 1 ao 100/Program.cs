d4d1727ef002e61f0759f06be1c8ad126925c262
Eiji <eijibrenno070@gmail.com>
2026-04-09 14:12:35 -0300

    Tabuada do 1 ao 100

---
+++ Tabuada do 1 ao 100/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Digite o número que você deseja ver a tabuada: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"\nTabuada do {numero} (do 1 ao 100):");
+Console.WriteLine("------------------------------");
+
+for (int i = 1; i <= 100; i++)
+{
+    int resultado = numero * i;
+
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+}
+
+Console.WriteLine("------------------------------");
+Console.WriteLine("Fim da tabuada. Aperte qualquer tecla para sair.");
