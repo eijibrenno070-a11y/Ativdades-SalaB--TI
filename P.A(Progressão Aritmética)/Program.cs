243554f23120865a7a0d3cc8e8c2ce1a4f0eaadf
Eiji <eijibrenno070@gmail.com>
2026-04-09 13:51:15 -0300

    Update Program.cs

--- P.A(Progressão Aritmética)/Program.cs
+++ P.A(Progressão Aritmética)/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int razão, termos, resultado = 0;
+Console.Write("Digite o nº de Termos: ");
+int.TryParse(Console.ReadLine()!, out termos);
+Console.Write("Digite o nº da razão: ");
+int.TryParse(Console.ReadLine()!, out razão);
+for (int i = 1; i <= termos; i++)
+{
+    resultado =  resultado + razão;
+    Console.Write($"{resultado}➡️");
+}
+Console.Write("Esses são os números");
