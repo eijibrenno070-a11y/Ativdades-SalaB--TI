4bbd266f28c526a4a625f95e2623142c51b8a8f1
Eiji <eijibrenno070@gmail.com>
2026-04-09 15:36:28 -0300

    Juros Simples

---
+++ Juros Simples/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("--- CÁLCULO DE JUROS SIMPLES ---");
+
+Console.Write("Digite o Capital (C): ");
+double capital = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a Taxa de Juros mensal (i) em %: ");
+double taxaPorcentagem = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o Tempo (t) em meses: ");
+int tempo = int.Parse(Console.ReadLine());
+
+double taxaDecimal = taxaPorcentagem / 100;
+
+double juros = capital * taxaDecimal * tempo;
+
+double montante = capital + juros;
+
+Console.WriteLine("\n--- RESULTADOS ---");
+Console.WriteLine($"Total de Juros: {juros}");
+Console.WriteLine($"Montante Final: {montante}");
+
+Console.WriteLine("\nCálculo finalizado.");
